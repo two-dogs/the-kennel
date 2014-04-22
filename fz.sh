bb5536aa351a026597e56c9e83d4855ec6da053c
Two_Dogs <escmail@comcast.net>
2014-04-21 20:02:51 -0700

    Update fz.sh

--- fz.sh
+++ fz.sh
@@ -1,8 +1,3 @@
-
-sudo apt-get install pastebinit
+sudo aptitude install pastebinit
 sudo inxi -U
-{ inxi -c0 -FRzdfulportcm20 ; egrep -i 'warn|error|critical' /var/log/syslog ; } | pastebinit
-
-
-
-
+{ inxi -c0 -FRfzdfulporxxtcm20 ; egrep -i 'warn|error|critical' /var/log/syslog ; egrep -i 'Kernel|conf|EE|WW' /var/log/Xorg.0.log ; } | pastebinit

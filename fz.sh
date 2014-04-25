b63f9baa9cc16b70c2039b5dbe20860df882be91
Two_Dogs <escmail@comcast.net>
2014-04-25 03:57:59 -0700

    Update fz.sh

--- fz.sh
+++ fz.sh
@@ -1,4 +1,4 @@
 #ver 2.0
 sudo aptitude install pastebinit
 sudo inxi -U
-{ inxi -c0 -FRfzdfulporxxtcm10 ; echo ---begin-syslog-- ; egrep -i 'warn|error|critical' /var/log/syslog |tail --lines=20 ; echo ---begin-Xorg-log--- ; egrep -i 'Kernel|conf|EE|WW' /var/log/Xorg.0.log ; echo ---begin-xession-log--- ; egrep -i 'error|fail' ~/.xsession-errors ; echo ---end--- ; } | pastebinit -a fz-script
+{ inxi -c0 -FRfzdfulporxxtcm10 ; echo ---begin-syslog-- ; egrep -i 'warn|error|critical' /var/log/syslog ; echo ---begin-Xorg-log--- ; egrep -i 'Kernel|conf|EE|WW' /var/log/Xorg.0.log ; echo ---begin-xession-log--- ; egrep -i 'error|fail' ~/.xsession-errors ; echo ---end--- ; } | pastebinit -a fz-script

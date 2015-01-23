a6e09497ccd5f1181d3a958551edf3678aebf1ee
Two_Dogs <escmail@comcast.net>
2015-01-22 21:21:30 -0800

    Delete fz.sh

--- fz.sh
+++
@@ -1,4 +0,0 @@
-#ver 2.0
-sudo aptitude install pastebinit
-sudo inxi -U
-{ inxi -c0 -FRfzdfulporxxtcm10 ; echo ---begin-Xorg-log--- ; egrep -i 'Kernel|conf|EE|WW' /var/log/Xorg.0.log ; echo ---begin-syslog-- ; egrep -i 'warn|error|critical' /var/log/syslog ;  echo ---begin-xession-log--- ; egrep -i 'error|fail' ~/.xsession-errors ; echo ---end--- ; } | pastebinit -a fz-script
